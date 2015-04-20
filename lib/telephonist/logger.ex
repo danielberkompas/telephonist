7d80ee8b30d50f60b8a5d57349f8249bef7ec48e
Daniel Berkompas <himself@danielberkompas.com>
2015-04-20 16:08:03 -0700

    Reword logging messages slightly

--- lib/telephonist/logger.ex
+++ lib/telephonist/logger.ex
@@ -7,19 +7,19 @@ defmodule Telephonist.Logger do
   end
 
   def on_event(:lookup_success, {sid, status, state}) do
-    log sid, "Call found in status #{inspect status} and state #{inspect state}"
+    log sid, "Call found in status #{inspect status} and #{inspect state}"
   end
 
   def on_event(:call_complete, {sid, _machine, twilio, options}) do
     log sid, "Call completed. Twilio: #{inspect twilio} Options: #{inspect options}"
   end
 
-  def on_event(:attempt_transition, {sid, state_machine, state_name, twilio, options}) do
-    log sid, "Calling #{state_machine}.transition(#{inspect state_name}, #{inspect twilio}, #{inspect options})"
+  def on_event(:attempt_transition, {sid, _state_machine, state_name, twilio, options}) do
+    log sid, "Calling transition(#{inspect state_name}, #{inspect twilio}, #{inspect options})"
   end
 
-  def on_event(:transition_error, {sid, exception, state_machine, state_name, twilio, options}) do
-    log sid, "Transition failed! Calling #{state_machine}.on_transition_error(#{inspect exception}, #{inspect state_name}, #{inspect twilio}, #{inspect options})"
+  def on_event(:transition_error, {sid, exception, _state_machine, state_name, twilio, options}) do
+    log sid, "Transition failed! Calling on_transition_error(#{inspect exception}, #{inspect state_name}, #{inspect twilio}, #{inspect options})"
   end
 
   def on_event(:next_state, {sid, _status, state}) do
