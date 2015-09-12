f7a37f787a4d3231eeb53fca8cbf0871be67715c
Daniel Berkompas <himself@danielberkompas.com>
2015-09-12 09:17:55 -0700

    Update ExTwiml dependency to 2.0.0

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Telephonist.Mixfile do
   defp deps do
     [
       {:immortal, "~> 0.2.0"},
-      {:ex_twiml, "~> 1.1.0"},
+      {:ex_twiml, "~> 2.0"},
       {:inch_ex, only: :docs},
       {:ex_doc, only: :docs}
     ]
